26644bd20fe4376f79c6456e9fd5da7c91371803
Hemanth <hemuchiluka3@gmail.com>
2017-09-10 02:33:07 -0400

    init method

--- pwm/src/main/java/com/example/androidthings/simplepio/PWMActivity.java
+++ pwm/src/main/java/com/example/androidthings/simplepio/PWMActivity.java
@@ -68,8 +68,8 @@ public class PWMActivity extends Activity implements
     private static final String TAG = PWMActivity.class.getSimpleName();
     private List<String> mVisitedIds;
     private String mUUID;
-    private boolean mIsParent=false;
-    private boolean mIsDiscoveryOn=false;
+    private boolean mIsParent;
+    private boolean mIsDiscoveryOn;
     private String mParentId;
     private TimerTask mBackPropTimer;
 
@@ -98,14 +98,22 @@ public class PWMActivity extends Activity implements
     private final SimpleArrayMap<Long, NotificationCompat.Builder> outgoingPayloads = new SimpleArrayMap<>();
 
 
+    private void init(){
+        mIsDiscoveryOn=false;
+        mIsParent=false;
+        mParentId=null;
+        mDidPing=false;
+        mVisitedIds=new ArrayList<>();
+        mChildMetrics=new ArrayList<>();
+        mAllowDiscovery=true;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pwm);
         activity=findViewById(R.id.activity_pwm);
-        mVisitedIds=new ArrayList<>();
-        mChildMetrics=new ArrayList<>();
-        mDidPing=false;
+        init();
         mUUID=UUID.randomUUID().toString();
         mGoogleApiClient = new GoogleApiClient.Builder(this)
                 .addConnectionCallbacks(this)
@@ -168,8 +176,10 @@ public class PWMActivity extends Activity implements
                             @Override
                             public void onResult(@NonNull Status status) {
                                 Log.d(TAG,status.toString());
+                                init();
                             }
                         });
+
             }
 
             @Override
@@ -397,7 +407,7 @@ public class PWMActivity extends Activity implements
                                                             });
                                         }
                                     };
-                                    new Timer().schedule(mBackPropTimer,12000);
+                                    new Timer().schedule(mBackPropTimer,10000);
                                 } else {
                                     // We were unable to start discovering.
                                 }
